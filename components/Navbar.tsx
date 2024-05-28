bddc8150f1e606b795fbe118f1b441a44b5c76b5
kaytee07 <kofi.taylor07@gmail.com>
2024-05-28 19:07:58 +0000

    creating site navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ({logo, navItems,}: { logo: string, navItems: string []}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
